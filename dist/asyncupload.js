b8d14b5cdb856beeef5a8d090346d8d37e4c0858
damianpolak <damianpolak.it@gmail.com>
2018-04-18 18:12:24 +0200

    Latest version 180420181812

--- dist/asyncupload.js
+++ dist/asyncupload.js
@@ -1,4 +1,16 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+/*
+ * asyncupload.js
+ * https://github.com/damianpolak/asyncupload.js
+ *
+ * Copyright 2017, Damian Polak
+ * lib used: Bootstrap 4.0.0, superbytes.js
+ *
+ * Licensed under the MIT license:
+ * https://opensource.org/licenses/MIT
+ *
+ */
+ 
 module.exports = ajax = () => {
   'use strict';
   return {
