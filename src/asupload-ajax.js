14d91465998fe52946490201c0156dabd3d50ed6
damianpolak <damianpolak.it@gmail.com>
2018-04-19 23:16:34 +0200

    Added two new events

--- src/asupload-ajax.js
+++ src/asupload-ajax.js
@@ -9,7 +9,7 @@
  * https://opensource.org/licenses/MIT
  *
  */
- 
+
 module.exports = ajax = () => {
   'use strict';
   return {
@@ -22,9 +22,22 @@ module.exports = ajax = () => {
         additional(e);
       }, false);
 
+      xhr.upload.addEventListener("error", () => {
+        console.log('TRANSFER FAILED!');
+      }, false);
+
+      xhr.upload.addEventListener("abort", () => {
+        console.log('TRANSFER CANCELED!');
+      }, false);
+
+      xhr.upload.addEventListener("loadend", () => {
+        console.log('The transfer finished (although we dont know if it succeeded or not).');
+      }, false);
+
       xhr.onreadystatechange = function() {
           if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
               // Action for success
+              console.log('success');
           }
       }
       xhr.send(data);
